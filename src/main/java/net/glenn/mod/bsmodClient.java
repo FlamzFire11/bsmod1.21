450ed9b661a150f81d2b7201472dcee8b67228c5
Glenn <glenn.liuchanghui@gmail.com>
2025-08-02 22:54:41 +0800

    Initial commit

---
+++ src/main/java/net/glenn/mod/bsmodClient.java
@@ -0,0 +1,9 @@
+package net.glenn.mod;
+import net.fabricmc.api.ClientModInitializer;
+
+public class bsmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

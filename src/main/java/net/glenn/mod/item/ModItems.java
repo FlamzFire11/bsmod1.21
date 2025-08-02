7015cee6fbc9530f061a9a159bd3c9d70569c643
Glenn <glenn.liuchanghui@gmail.com>
2025-08-03 00:17:02 +0800

    end of vid 2

--- src/main/java/net/glenn/mod/item/ModItems.java
+++ src/main/java/net/glenn/mod/item/ModItems.java
@@ -8,14 +8,20 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(Bsmod.MOD_ID  + name), item);
+        return Registry.register(Registries.ITEM, Identifier.of(Bsmod.MOD_ID, name), item);
     }
+
     public static void registerModItems() {
-        Bsmod.LOGGER.info("Registering ModItems for "+ Bsmod.MOD_ID);
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(PINK_GARNET));
+        Bsmod.LOGGER.info("Registering Mod Items for " + Bsmod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
     }
 }
 

6250927e91e19313a9ead8bd775d1f0434916d40
Glenn <glenn.liuchanghui@gmail.com>
2025-08-02 23:16:36 +0800

    a

--- src/main/java/net/glenn/mod/Bsmod.java
+++ src/main/java/net/glenn/mod/Bsmod.java
@@ -2,6 +2,7 @@ package net.glenn.mod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.glenn.mod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class Bsmod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-	}
+		ModItems.registerModItems();
+    }
 }
---
+++ src/main/java/net/glenn/mod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.glenn.mod.item;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.glenn.mod.Bsmod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Bsmod.MOD_ID  + name), item);
+    }
+    public static void registerModItems() {
+        Bsmod.LOGGER.info("Registering ModItems for "+ Bsmod.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(PINK_GARNET));
+    }
+}
+

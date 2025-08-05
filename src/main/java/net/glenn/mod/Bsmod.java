ce1844bfb4d665c582f59f8427a6cf21aa5d514a
Glenn <glenn.liuchanghui@gmail.com>
2025-08-05 08:38:15 +0800

    end of vid 3

--- src/main/java/net/glenn/mod/Bsmod.java
+++ src/main/java/net/glenn/mod/Bsmod.java
@@ -2,16 +2,18 @@ package net.glenn.mod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.glenn.mod.block.ModBlocks;
 import net.glenn.mod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Bsmod implements ModInitializer {
-		public static final String MOD_ID = "bsmod";
+	public static final String MOD_ID = "bsmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
-    }
+		ModBlocks.registerModBlocks();
+	}
 }
